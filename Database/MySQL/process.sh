d968d4766adb37814967666ca1c63054ba90dfd9
KRNelson <kevin92620@yahoo.com>
2022-09-04 23:44:16 -0700

    More cleanup

--- Database/MySQL/process.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TARGET=/etc/reports
-
-inotifywait -m -e create -e moved_to --format "%f" $TARGET \
-        | while read FILENAME
-                do
-                    echo $FILENAME
-                done
