f372eac28998ffd222bf22254b7c02a0b1168077
KRNelson <kevin92620@yahoo.com>
2022-09-26 17:46:53 -0700

    Travis-CI

--- Web/Apache/Elm/src/Upload.elm
+++ Web/Apache/Elm/src/Upload.elm
@@ -1,14 +1,14 @@
 module Upload exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text, form, input, iframe)
-import Html.Events exposing (onClick)
 
 import File exposing (File)
 import File.Select as Select
-import Html exposing (Html, button, p, text)
-import Html.Attributes exposing (style, type_, action, method, enctype, name, multiple, required, value)
+
+import Html exposing (Html, button, div, text, form, input, iframe, p, span)
+import Html.Attributes exposing (attribute, class, style, type_, value, hidden)
 import Html.Events exposing (onClick)
+
 -- import Task
 import Json.Decode as Decode
 import Http
@@ -26,12 +26,12 @@ main =
 
 -- MODEL
 type alias Model =
-  { log : Maybe String
-  , files : List File
+  { files : List File
+  , result : Maybe (Result Http.Error (String))
   }
 
 default : Model
-default = Model Nothing []
+default = Model [] Nothing
 
 init : () -> (Model, Cmd Msg)
 init _ =
@@ -40,25 +40,29 @@ init _ =
 -- UPDATE
 type Msg
   = LogRequested
-  -- | LogSelected File
   | LogLoaded File (List File)
+  | LogUpload
   | LogUploaded (Result Http.Error (String))
+  | RemoveFile String
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     LogRequested ->
       ( model
-      , Select.files ["application/zevtc"] LogLoaded -- LogSelected
+      , Select.files ["application/zevtc"] LogLoaded
       )
 
     LogLoaded file files ->
-      ( model
+      ( {model | files = file :: (files ++ model.files)}, Cmd.none)
+
+    LogUpload -> 
+      (model
       , Http.request
         { method = "POST"
         , headers = []
         , url = "http://localhost:3000/upload"
-        , body = Http.multipartBody (Http.filePart "logs" file :: (List.map (\f -> Http.filePart "logs" f) files))
+        , body = Http.multipartBody ((List.map (\f -> Http.filePart "logs" f) model.files))
         , expect = Http.expectJson LogUploaded (Decode.field "message" Decode.string)
         , timeout = Nothing
         , tracker = Nothing
@@ -66,13 +70,16 @@ update msg model =
       )
 
     LogUploaded result ->
+      ({model | result = Just result}, Cmd.none)
+  
+{-
       case result of
           Ok message ->
             ({model | log = Just message}, Cmd.none)
           Err error ->
             case error of
                 Http.BadUrl url ->
-                  ({model | log = Just url}, Cmd.none)
+                  ({model | log = Just ("BadUrl: " ++ url)}, Cmd.none)
                 Http.Timeout ->
                   ({model | log = Just "Timeout"}, Cmd.none)
                 Http.NetworkError ->
@@ -80,27 +87,68 @@ update msg model =
                 Http.BadStatus _ -> 
                   ({model | log = Just "BadStatus"}, Cmd.none)
                 Http.BadBody body -> 
-                  ({model | log = Just body}, Cmd.none)
+                  ({model | log = Just ("BadBody: " ++ body)}, Cmd.none)
+-}
+
+    RemoveFile filename ->
+      ({model | files = List.filter(\file -> File.name file /= filename) model.files }, Cmd.none )
 
 -- VIEW
+
+viewFileSelected : String -> Html Msg
+viewFileSelected filename = 
+  div [ class "alert alert-success alert-dismissible fade show w-50" 
+      , attribute "role" "alert"
+      ]
+    [ text filename
+    , button [type_ "button"
+             , class "btn close"
+             , attribute "data-dismiss" "alert"
+             , attribute "aria-label" "Close"
+             , onClick <| RemoveFile filename
+             ]
+        [ span [attribute "aria-hidden" "true"] [ text "x" ]
+        ]
+    ]
+
+viewForm : Html Msg
+viewForm = 
+  form []
+    [ input [type_ "button", onClick LogRequested, value "Load Log"] []
+    ]
+
+viewUpload : Bool -> Html Msg
+viewUpload hide =
+  div [ hidden hide ]
+    [ button [type_ "button", onClick LogUpload ] [ text "Upload!"]
+    ]
+
+viewContent : String -> Html Msg
+viewContent content =
+  p [style "white-space" "pre" ] [ text content ]
+
 view : Model -> Html Msg
 view model =
-  case model.log of
+  case model.result of
     Nothing ->
         div []
-        [ form []
-        [
-            input [ type_ "button", onClick LogRequested, value "Load Log" ] []
-        ]
+        [ viewForm 
+        , div [class ""] <| List.map (\file -> viewFileSelected <| File.name file) model.files
+        , viewUpload <| List.isEmpty model.files
         ]
 
-    Just content ->
-        div []
-        [ p [ style "white-space" "pre" ] [ text content ]
-        , form []
-            [ input [type_ "button", onClick LogRequested, value "Load Log"] []
-            ]
-        ]
+    Just result ->
+      case result of
+        Ok message ->
+          div []
+          [ viewContent message
+          , viewForm
+          ]
+        Err _ ->
+          div []
+          [ viewContent "Error"
+          , viewForm
+          ]
 
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
---
+++ Web/Apache/Elm/tests/UploadTest.elm
@@ -0,0 +1,176 @@
+module UploadTest exposing (..)
+
+import Upload as U
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector as Selector
+
+import Html exposing (Html, button, p, text, input, form, div)
+import Html.Attributes exposing (style, type_, action, method, enctype, name, multiple, required, value)
+import Html.Events exposing (onClick)
+
+testBootstrapAlert : (String -> Html msg) -> Test
+testBootstrapAlert html =
+    describe "Test for Bootstrap alert. "
+        [ test "Expect Cancel" <|
+            \_ ->
+                let
+                    content = "Hello World"
+                    query = [ Selector.text "x", Selector.classes ["btn", "close"] ]
+                in
+                    html content
+                        |> Query.fromHtml
+                        |> Query.has query
+        , fuzzWith { runs = 10000 } string "Fuzz expect cancel" <|
+            \randomContent -> 
+                let
+                    query = [ Selector.text "x", Selector.classes ["btn", "close"] ]
+                in
+                    html randomContent
+                        |> Query.fromHtml
+                        |> Query.has query
+        ]
+
+suite : Test
+suite =
+    describe "The Upload Module"
+        [ describe "Upload.default"
+            [ test "Expected Default" <|
+                \_ ->
+                    let
+                        expected = (U.Model [] Nothing)
+                    in
+                        U.default
+                            |> Expect.equal expected
+            ]
+        , describe "Upload.init"
+            [ test "Init default model" <|
+                \_ ->
+                    let
+                        (model, _) = U.init ()
+                        expected = U.default
+                    in
+                        model 
+                            |> Expect.equal expected
+            , test "Init no commands" <|
+                \_ ->
+                    let
+                        (_, command) = U.init ()
+                        expected = Cmd.none
+                    in
+                        command 
+                            |> Expect.equal expected
+            ]
+        , describe "Upload.viewFileSelected"
+            [ skip <| test "Expected File Selected" <|
+                \_ ->
+                    let
+                        content = "Hello World"
+                        expected = div [] [ text content ]
+                    in
+                        U.viewFileSelected content
+                            |> Expect.equal expected
+            , test "Expect Content" <|
+                \_ ->
+                    let
+                        content = "Hello World"
+                        query = [ Selector.text content ]
+                    in
+                        U.viewFileSelected content
+                            |> Query.fromHtml
+                            |> Query.has query
+            , fuzz string "Fuzz Content" <|
+                \randomContent ->
+                    let
+                        query = [ Selector.text randomContent ]
+                    in
+                        U.viewFileSelected randomContent
+                            |> Query.fromHtml
+                            |> Query.has query
+            , let 
+                content = "Hello World"
+              in 
+                testBootstrapAlert U.viewFileSelected
+            ]
+
+        , describe "Uplaod.viewFileUploading"
+            [ todo "Write a test for the HTML while a file is uploading. "
+            ]
+        , describe "Upload.viewFileUploaded"
+            [ todo "Write a test for the HTML after a file is uploaded. "
+            ]
+        , describe "Upload.viewFileUploadFailed"
+            [ todo "Write a test for the HTML if the upload fails. "
+            ]
+        , describe "Upload.viewForm"
+            [ test "Expected Form" <|
+                \_ ->
+                    let
+                        expected =   form [] [ input [type_ "button", onClick U.LogRequested, value "Load Log"] [] ]
+                    in
+                        U.viewForm
+                            |> Expect.equal expected
+            ]
+        , describe "Upload.viewContent"
+            [ test "Expected Content" <|
+                \_ ->
+                    let
+                        content = "Hello World"
+                        expected = p [style "white-space" "pre" ] [ text content ]
+                    in
+                        U.viewContent content
+                            |> Expect.equal expected 
+            , fuzz string "Fuzzy Content" <|
+                \randomContent ->
+                    let
+                        expected = p [style "white-space" "pre" ] [text randomContent]
+                    in
+                        U.viewContent randomContent
+                            |> Expect.equal expected
+            ]
+        , describe "Upload.view"
+            [ skip <| test "Expected Default" <|
+                \_ -> 
+                    let
+                        expected = div [] [ U.viewForm ]
+                        model = U.default
+                    in 
+                        U.view model
+                            |> Expect.equal expected
+            , skip <| test "Expected Nothing" <|
+                \_ -> 
+                    let
+                        expected = div [] [ U.viewForm ]
+                        model = U.Model Nothing []
+                    in 
+                        U.view model
+                            |> Expect.equal expected
+            , test "Expected Content" <|
+                \_ -> 
+                    let
+                        content = "Hello World"
+                        model = (U.Model (Just content) [])
+                        expected = div [] [ U.viewContent content, U.viewForm ]
+                    in 
+                        U.view model
+                            |> Expect.equal expected
+            , fuzz string "Fuzzy Content" <|
+                \randomContent ->
+                    let
+                        model = (U.Model (Just randomContent) [])
+                        expected = div [] [U.viewContent randomContent, U.viewForm]
+                    in 
+                        U.view model
+                            |> Expect.equal expected
+            ]
+        , describe "Upload.subscriptions"
+            [ test "No subscriptions" <|
+                \_ ->
+                    U.default 
+                        |> U.subscriptions
+                        |> Expect.equal Sub.none
+            ]
+        ]
